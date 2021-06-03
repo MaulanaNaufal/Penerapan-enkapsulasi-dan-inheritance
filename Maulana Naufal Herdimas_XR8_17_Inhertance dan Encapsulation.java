65d74cbe943f02710e74172c54376b23f8272cee
MaulanaNaufal <82005991+MaulanaNaufal@users.noreply.github.com>
2021-06-03 09:25:17 +0700

    Add files via upload

---
+++ Maulana Naufal Herdimas_XR8_17_Inhertance dan Encapsulation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-1
+ */
+package InheritanceXEnkapsulation;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author E
+ */
+public class InheritancexEncaptulation {
+    void identitas(String TanggalLahir, String Nama, int Kelas, int Umur){
+        System.out.println("Nama : " +Nama );
+        System.out.println("TanggalLahir :" +TanggalLahir); 
+        System.out.println("Kelas " +Kelas);
+        System.out.println("Umur : " +Umur);
+    }
+}
